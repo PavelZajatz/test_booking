42022dec4c9f89b8e4d7b3c733afec6d932088fa
Pavluha44 <52718431+Pavluha44@users.noreply.github.com>
2020-04-29 19:53:16 +0300

    updated is present

--- pages/base_page.py
+++ pages/base_page.py
@@ -9,22 +9,32 @@ class BasePage():
     def __init__(self, browser, link, timeout=2):
         self.browser = browser
         self.link = link
-        self.browser.implicitly_wait(timeout)
+
 
     def open(self):
         self.browser.get(self.link)
 
-
+    '''
     def is_element_present(self, how, what): #базовый метод проверки что элемент присутствует
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
             return False
         return True
+    '''
+
+
 
     def is_element_visible(self, how, what, timeout=3): #базовый метод проверки что элемент присутствует
         try:
             WebDriverWait(self.browser, timeout).until(EC.visibility_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
+    def is_element_present(self, how, what, timeout=3):  # базовый метод проверки что элемент присутствует
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
         return True
