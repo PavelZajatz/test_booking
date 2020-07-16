72fa62b48f51a938ec8b20ce8ef94dc5d89ca8e2
Pavluha44 <52718431+Pavluha44@users.noreply.github.com>
2020-07-16 22:42:01 +0300

    p

--- conftest.py
+++ conftest.py
@@ -1,8 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
+
 
 import time
 
